7f24d942f8b85f5a58d84c6aeb9665cb7451f8cd
Braian Vaylet <braianvaylet@gmail.com>
2022-07-16 10:10:22 -0300

    mas opcional

--- 2.0_Instituto.sol
+++ 2.0_Instituto.sol
@@ -16,72 +16,103 @@
 //  Joan   |    0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db    |      9, 9, 9
 //  Maria  |    0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB    |      2, 6, 7
 
-
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Instituto {
+    using SafeMath for uint256;
 
-    using SafeMath for uint;
-    
-    address _owner;        
+    address _owner;
 
-    mapping (address => uint[]) Notas;
-    
-    event NotaCargada(address alumno, uint nota, string mensaje);
+    mapping(address => uint256[]) Notas;
+
+    event NotaCargada(address alumno, uint256 nota, string mensaje);
 
     constructor() {
         _owner = msg.sender;
     }
 
-    function cargaDeNota(address alumno, uint nota) NotaValida(nota) public UnicamenteInstituto {         
+    function cargaDeNota(address alumno, uint256 nota)
+        public
+        NotaValida(nota)
+        UnicamenteInstituto
+    {
         Notas[alumno].push(nota);
         emit NotaCargada(alumno, nota, "Se cargo una nota a un alumno");
     }
 
-    function notasDeAlumno(address alumno) public view UnicamenteAlumnoInstituto(alumno) returns (address, uint[] memory notas) {        
-        return (
-            alumno, 
-            Notas[alumno]
-        );
+    function notasDeAlumno(address alumno)
+        public
+        view
+        UnicamenteAlumnoInstituto(alumno)
+        returns (address, uint256[] memory notas)
+    {
+        return (alumno, Notas[alumno]);
     }
 
-    function promedioDeAlumno(address alumno) public view UnicamenteAlumnoInstituto(alumno) returns (address, uint notas) {       
-        uint sum;
-        for (uint i = 0; i < Notas[alumno].length; i++) {
-            sum = sum +  Notas[alumno][i];
+    function promedioDeAlumno(address alumno)
+        public
+        view
+        UnicamenteAlumnoInstituto(alumno)
+        returns (address, uint256 notas)
+    {
+        uint256 sum;
+        for (uint256 i = 0; i < Notas[alumno].length; i++) {
+            sum = sum + Notas[alumno][i];
         }
-        return (
-            alumno, 
-            sum / Notas[alumno].length
-        );
-    }    
+        return (alumno, sum / Notas[alumno].length);
+    }
+
+    function notasDeAlumnoSinPermisos(address alumno)
+        public
+        view
+        returns (address, uint256[] memory notas)
+    {
+        return (alumno, Notas[alumno]);
+    }
+
+    function promedioDeAlumnoSinPermisos(address alumno)
+        public
+        view
+        returns (address, uint256 notas)
+    {
+        uint256 sum;
+        for (uint256 i = 0; i < Notas[alumno].length; i++) {
+            sum = sum + Notas[alumno][i];
+        }
+        return (alumno, sum / Notas[alumno].length);
+    }
 
     // Usando SafeMath
-    function SafeMath_promedioDeAlumno(address alumno) public view UnicamenteAlumnoInstituto(alumno) returns (address, uint notas) {        
-        uint sum;
-        for (uint i = 0; i < Notas[alumno].length; i++) {
+    function SafeMath_promedioDeAlumno(address alumno)
+        public
+        view
+        UnicamenteAlumnoInstituto(alumno)
+        returns (address, uint256 notas)
+    {
+        uint256 sum;
+        for (uint256 i = 0; i < Notas[alumno].length; i++) {
             sum = sum.add(Notas[alumno][i]);
-        }        
-        return (
-            alumno, 
-            sum.div(Notas[alumno].length)
-        );
+        }
+        return (alumno, sum.div(Notas[alumno].length));
     }
-    
+
     // Validaciones
-    modifier UnicamenteInstituto {
+    modifier UnicamenteInstituto() {
         require(_owner == msg.sender, "No tienes permisos de administrador");
         _;
     }
 
     modifier UnicamenteAlumnoInstituto(address alumno) {
-        require(alumno == msg.sender || _owner == msg.sender, "No tienes permisos suficientes");
+        require(
+            alumno == msg.sender || _owner == msg.sender,
+            "No tienes permisos suficientes"
+        );
         _;
     }
 
-    modifier NotaValida(uint nota) {
+    modifier NotaValida(uint256 nota) {
         require(nota >= 0 && nota <= 10, "La nota ingresada es incorrecta");
         _;
     }
