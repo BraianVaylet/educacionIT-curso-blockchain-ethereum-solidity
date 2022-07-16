628ccb1d5f851edb5975c22a7b58ef0551b03eea
Braian Vaylet <braianvaylet@gmail.com>
2022-07-16 12:55:23 -0300

    .

---
+++ Herencia.sol
@@ -0,0 +1,11 @@
+// Ejercicio:
+// Definir un molde de una figura geometrica generica. (usar contracto abstraco o interface)
+// (pista: modelar perimetro y superficie.)
+// Hacer al menos 2 figuras.
+// deploy de una figura geometrica y poder calcular su perimetro y superficie
+
+// Ejercicio:
+// Definir un molde de una figura geometrica generica. (usar contracto abstraco o interface)
+// (pista: modelar perimetro y superficie.)
+// Hacer al menos 2 figuras.
+// deploy de una figura geometrica y poder calcular su perimetro y superficie
